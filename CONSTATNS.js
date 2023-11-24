1cec56c321dda1a77463b78daaecb3926111be50
venkateshMorishetti <117025754+venkateshMorishetti@users.noreply.github.com>
2023-11-25 00:45:06 +0530

    reverted key

---
+++ CONSTATNS.js
@@ -0,0 +1,60 @@
+export const netflixlogo = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const userAvatar = "https://occ-0-4857-2186.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABT980E7afJBlYZzvyMECBb6z3BkkG0XqY_oXyYfxim5c7pcrWM0lRzT-cKIImpMzTOhbBGslrEIb7ORMvOpHdkWMU4109Eo.png?r=2c8";
+
+export const loginPageBackground = "https://assets.nflxext.com/ffe/siteui/vlv3/77d35039-751f-4c3e-9c8d-1240c1ca6188/cf244808-d722-428f-80a9-052acdf158ec/IN-en-20231106-popsignuptwoweeks-perspective_alpha_website_small.jpg";
+
+export const API_HEADERS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTg4ZThmODE5NTM5MTBkYzBlMDNjMjM3YzU0MDJlNyIsInN1YiI6IjY1NWQ2YzM4MjY2Nzc4MDBjYTUzMmEzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-1JSksQy_unsvz2jOakhIkDZx4GOfjGAb2YBXc30j2U'
+    }
+  }
+
+export const MOVIE_IMAGE_BASE_URI = 'https://image.tmdb.org/t/p/w500/';
+
+export const LANGUAGE_CONFIG = [
+  {
+    key: "en",
+    label: "English"
+  },
+  {
+    key: "hi",
+    label: "Hindi"
+  },
+  {
+    key: "ta",
+    label: "Tamil"
+  },
+  {
+    key: "te",
+    label: "Telugu"
+  }
+
+]
+
+
+export const COMPONENTS_CONFIG = [
+  {
+    routeURL: "/movies",
+    label: "Movies"
+  },
+  {
+    routeURL: "/tv-shows",
+    label: "TV Shows"
+  },
+  {
+    routeURL: "/news&popular",
+    label: "News & Popular"
+  },
+  {
+    routeURL: "/my-list",
+    label: "My List"
+  },
+  {
+    routeURL: "/browse-by-language",
+    label: "Browse By Language"
+  }
+]
+
